9b798544bfb0d019de057f02c5183394ffd43a49
gozdeyildirmaz <gozdeyildirmaz@gmail.com>
2021-01-18 21:14:26 +0300

    -concurrently eklendi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBp_ZUNDATWV-dTSoQp-Ik6PyVWjozg_C8',
+    authDomain: 'angularconnection-b3402.firebaseapp.com',
+    databaseURL: 'https://angularconnection-b3402-default-rtdb.firebaseio.com',
+    projectId: 'angularconnection-b3402',
+    storageBucket: 'angularconnection-b3402.appspot.com',
+    messagingSenderId: '408806871063',
+    appId: '1:408806871063:web:37eb77e3f5a692a46f4c52'
+  }
 };
